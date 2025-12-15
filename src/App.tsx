5d2654f9f8b12f8cc64af7c6dba9deba7eb1e41b
Ahtsham Abbasi <a_abbasi@flo.health>
2025-12-15 03:18:30 +0100

    Refactor App.tsx to remove React import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import { useState, useMemo } from 'react';
 import { MortgageInputs, calculateComparison } from './utils/mortgageCalculations';
 import { InputForm } from './components/InputForm';
 import { ComparisonSummary } from './components/ComparisonSummary';
